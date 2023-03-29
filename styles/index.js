36bb6c3b0b14a4c120fe9310ec79ef9cf7346af6
Bastien Ricard <bastien.ricard-dev@outlook.com>
2023-03-29 18:41:13 +0200

    Add hero section

--- styles/index.js
+++ styles/index.js
@@ -15,9 +15,9 @@ const styles = {
 
   // hero section
   heroHeading:
-      'font-bold lg:text-[144px] md:text-[100px] sm:text-[60px] text-[44px] lg:leading-[158.4px] md:leading-[114.4px] sm:leading-[74.4px] leading-[64.4px] uppercase text-white',
+    'font-bold lg:text-[144px] md:text-[100px] sm:text-[60px] text-[44px] lg:leading-[158.4px] md:leading-[114.4px] sm:leading-[74.4px] leading-[64.4px] uppercase text-white',
   heroDText:
-      'md:w-[212px] sm:w-[80px] w-[60px] md:h-[108px] sm:h-[48px] h-[38px] md:border-[18px] border-[9px] rounded-r-[50px] border-white sm:mx-2 mx-[6px]',
+    'md:w-[212px] sm:w-[80px] w-[60px] lg:h-[108px] md:h-[74px] sm:h-[45px] h-[34px] lg:border-[18px] md:border-[14px] border-[7px] rounded-r-[50px] border-white sm:mx-2 mx-[6px] md:mt-[8px] sm:mt-[4px] mt-[1px]',
 };
 
 export default styles;
