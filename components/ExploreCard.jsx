48703121600b1463721636333ced99dbf6041dfa
Bastien Ricard <bastien.ricard-dev@outlook.com>
2023-04-03 18:55:03 +0200

    Fix error url for explore section images & favicon

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -13,11 +13,7 @@ const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => (
     } flex items-center justify-center min-w-[170px] h-[700px] transition-[flex] duration-[0.7s] ease-out cursor-pointer`}
     onClick={() => handleClick(id)}
   >
-    <img
-      src={`.${imgUrl}`}
-      alt={title}
-      className="absolute w-full h-full object-cover rounded-[24px]"
-    />
+    <img src={imgUrl} alt={title} className="absolute w-full h-full object-cover rounded-[24px]" />
     {active !== id ? (
       <h3 className="font-semibold sm:text-[26px] text-[18px] text-white absolute z-0 lg:bottom-20 lg:rotate-[-90deg] lg:origin-[0,0]">
         {title}
